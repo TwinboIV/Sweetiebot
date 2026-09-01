951a5df63ff2b16ed90df68890e51e772304ebc7
Anna Towle <theannatowle@gmail.com>
2026-09-01 19:12:34 -0400

    lol

--- src/sweetiebot.py
+++ src/sweetiebot.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 load_dotenv()
 #intents.message_content = True
-TARGET_TIME = time(hour=23, minute=10, second=0)
+TARGET_TIME = time(hour=23, minute=13, second=0)
 base_path = "..\\res\\weekdays\\"
 last_images = {
     "Monday": "",
